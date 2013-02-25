3ccfec10c4f4783cfd9548b2624cef675909ad80
iuribtt@gmail.com <iuribtt@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2013-02-25 02:36:54 +0000

    Alteração no Index e criação de Models/DiscenteModel.cs. - Iúri

--- Codigo/VirtualPatient/VirtualPatient/Controllers/HomeController.cs
+++ Codigo/VirtualPatient/VirtualPatient/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VirtualPatient.Models;
 
 namespace VirtualPatient.Controllers
 {
     public class HomeController : Controller
     {
+
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -25,5 +28,16 @@ namespace VirtualPatient.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(DiscenteModel discente)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("About", discente);
+            }
+
+            return View();
+
+        }
     }
 }
