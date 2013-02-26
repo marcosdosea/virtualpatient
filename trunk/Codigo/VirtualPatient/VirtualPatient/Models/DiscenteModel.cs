97414923814bffcb28e9b111fc1b5b5324ef772f
iuribtt@gmail.com <iuribtt@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2013-02-26 23:30:03 +0000

    Alteração no Index e criação de Models/DiscenteModel.cs. - Iúri

---
+++ trunk/Codigo/VirtualPatient/VirtualPatient/Models/DiscenteModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VirtualPatient.Models
+{
+    public class DiscenteModel
+    {
+
+        [Required(ErrorMessage = "Por favor insira um número")]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }//ID do Aluno
+
+        [Required(ErrorMessage = "Por favor insira seu nome")]
+        [Display(Name = "Primeiro nome")]
+        public string PrimeiroNome { get; set; }//Nome
+
+        [Display(Name = "Sobre nome")]
+        public string SobreNome { get; set; }//Sobre Nome
+
+        [Required(ErrorMessage = "Insirá um email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Insira um e-mail valido")]
+        public string Email { get; set; }//Email
+    }
+}
