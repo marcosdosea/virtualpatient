7936ff1b4560769881ffa414bc0eb8b90137dac3
marcosdosea <marcosdosea@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2013-03-04 13:51:48 +0000

    versão com o módulo turma em funcionamento

--- trunk/Codigo/PacienteVirtual/PacienteVirtual/Models/TurmaModel.cs
+++ trunk/Codigo/PacienteVirtual/PacienteVirtual/Models/TurmaModel.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PacienteVirtual.Models
 {
     public class TurmaModel
     {
+        [Required]
+        [Display(Name = "Turma: ")]
         public long Id { get; set; }
+
+        [Required]
+        [Display(Name = "Código: ")]
         public string Codigo { get; set; }
+
+        [Required]
+        [Display(Name = "Período: ")]
         public string Periodo { get; set; }
+
+        //[Required]
+        [Display(Name = "Tutor: ")]
         public string Tutor { get; set; }
+
+        [Required]
+        [Display(Name = "Disciplina: ")]       
         public string NomeDisciplina { get; set; }
     }
 }
