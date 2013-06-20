b4944b185775f8142c96ea28f9bdedff6456cdd3
clevertonmaggot@gmail.com <clevertonmaggot@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2013-06-20 23:24:14 +0000

    Cadastros de MedicamentoNaoPrescrito e MedicamentoPrescrito

---
+++ Codigo/PacienteVirtual - Update base pelo VS2010/PacienteVirtual/Models/MedicamentoPrescritoModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using PacienteVirtual.App_GlobalResources;
+
+namespace PacienteVirtual.Models
+{
+    public class MedicamentoPrescritoModel
+    {
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "campo_requerido")]
+        [Display(Name = "consulta_variavel"/**, ResourceType = typeof(Mensagem)*/)]
+        public Int64 IdConsultaVariavel { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "campo_requerido")]
+        [Display(Name = "medicamento_codigo", ResourceType = typeof(Mensagem))]
+        public Int32 IdMedicamento { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "campo_requerido")]
+        [Display(Name = "fitoterapico"/**, ResourceType = typeof(Mensagem)*/)]
+        public bool Fitoterapico { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "campo_requerido")]
+        [Display(Name = "dosagem"/**, ResourceType = typeof(Mensagem)*/)]
+        public string Dosagem { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "campo_requerido")]
+        [Display(Name = "posologia"/**, ResourceType = typeof(Mensagem)*/)]
+        public string Posologia { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "campo_requerido")]
+        [Display(Name = "prescritor"/**, ResourceType = typeof(Mensagem)*/)]
+        public string Prescritor { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "campo_requerido")]
+        [Display(Name = "especialidade"/**, ResourceType = typeof(Mensagem)*/)]
+        public string Especialidade { get; set; }
+    }
+}
---
+++ Codigo/PacienteVirtual - Update base pelo VS2010/PacienteVirtual/Views/MedicamentoPrescrito/Details.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.master" Inherits="System.Web.Mvc.ViewPage<PacienteVirtual.Models.MedicamentoPrescritoModel>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Details
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+<h2>Details</h2>
+
+<fieldset>
+    <legend>MedicamentoPrescritoE</legend>
+
+    <div class="display-label">Fitoterapico</div>
+    <div class="display-field">
+        <%: Html.DisplayFor(model => model.Fitoterapico) %>
+    </div>
+
+    <div class="display-label">Dosagem</div>
+    <div class="display-field">
+        <%: Html.DisplayFor(model => model.Dosagem) %>
+    </div>
+
+    <div class="display-label">Posologia</div>
+    <div class="display-field">
+        <%: Html.DisplayFor(model => model.Posologia) %>
+    </div>
+
+    <div class="display-label">Prescritor</div>
+    <div class="display-field">
+        <%: Html.DisplayFor(model => model.Prescritor) %>
+    </div>
+
+    <div class="display-label">Especialidade</div>
+    <div class="display-field">
+        <%: Html.DisplayFor(model => model.Especialidade) %>
+    </div>
+</fieldset>
+<p>
+    <%: Html.ActionLink("Edit", "Edit", new { /* id=Model.PrimaryKey */ }) %> |
+    <%: Html.ActionLink("Back to List", "Index") %>
+</p>
+
+</asp:Content>
