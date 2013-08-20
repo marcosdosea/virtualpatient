12bb48ba9fb7a033b801ae0a80d1354519dcf6f9
marcosdosea@gmail.com <marcosdosea@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2013-08-20 19:15:19 +0000

    Organização de pastas do sistema

---
+++ trunk/Codigo/PacienteVirtual - Update base pelo VS2010/PacienteVirtual/Negocio/Consulta/GerenciadorAcaoQueixa.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PacienteVirtual.Models.Data;
+using Persistence;
+
+namespace PacienteVirtual.Models.Negocio
+{
+    public class GerenciadorAcaoQueixa
+    {
+        private static GerenciadorAcaoQueixa gAcaoQueixa;
+
+        private GerenciadorAcaoQueixa()
+        {
+        }
+
+        public static GerenciadorAcaoQueixa GetInstance()
+        {
+            if (gAcaoQueixa == null)
+            {
+                gAcaoQueixa = new GerenciadorAcaoQueixa();
+            }
+            return gAcaoQueixa;
+        }
+
+        /// <summary>
+        /// Insere dados do AcaoQueixa
+        /// </summary>
+        /// <param name="acaoQueixa"></param>
+        /// <returns></returns>
+        public int Inserir(AcaoQueixaModel acaoQueixa)
+        {
+            var repAcaoQueixa = new RepositorioGenerico<AcaoQueixaE>();
+            AcaoQueixaE _acaoQueixaE = new AcaoQueixaE();
+            try
+            {
+                Atribuir(acaoQueixa, _acaoQueixaE);
+
+                repAcaoQueixa.Inserir(_acaoQueixaE);
+                repAcaoQueixa.SaveChanges();
+
+                return _acaoQueixaE.IdAcaoQueixa;
+            }
+            catch (Exception e)
+            {
+                throw new DadosException("AcaoQueixa", e.Message, e);
+            }
+
+        }
+
+        /// <summary>
+        /// Atualiza dados do acaoQueixa
+        /// </summary>
+        /// <param name="acaoQueixa"></param>
+        public void Atualizar(AcaoQueixaModel acaoQueixa)
+        {
+            try
+            {
+                var repAcaoQueixa = new RepositorioGenerico<AcaoQueixaE>();
+                AcaoQueixaE _acaoQueixaE = repAcaoQueixa.ObterEntidade(d => d.IdAcaoQueixa == acaoQueixa.IdAcaoQueixa);
+                Atribuir(acaoQueixa, _acaoQueixaE);
+
+                repAcaoQueixa.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new DadosException("AcaoQueixa", e.Message, e);
+            }
+        }
+
+        /// <summary>
+        /// Remove dados do AcaoQueixa
+        /// </summary>
+        /// <param name="idAcaoAlternativa"></param>
+        public void Remover(int idAcaoQueixa)
+        {
+            try
+            {
+                var repAcaoQueixa = new RepositorioGenerico<AcaoQueixaE>();
+                repAcaoQueixa.Remover(d => d.IdAcaoQueixa == idAcaoQueixa);
+                repAcaoQueixa.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new DadosException("AcaoQueixa", e.Message, e);
+            }
+        }
+
+        /// <summary>
+        /// Consulta para retornar dados da entidade
+        /// </summary>
+        /// <returns></returns>
+        private IQueryable<AcaoQueixaModel> GetQuery()
+        {
+            var repAcaoQueixa = new RepositorioGenerico<AcaoQueixaE>();
+            var pvEntities = (pvEntities)repAcaoQueixa.ObterContexto();
+            var query = from acaoQueixa in pvEntities.tb_acao_queixa
+                        select new AcaoQueixaModel
+                        {
+                            IdAcaoQueixa = acaoQueixa.IdAcaoQueixa,
+                            DescricaoAcao = acaoQueixa.DescricaoAcao
+                        };
+            return query;
+        }
+
+        /// <summary>
+        /// Obtém todos os acaoQueixa cadastrados
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<AcaoQueixaModel> ObterTodos()
+        {
+            return GetQuery().ToList();
+        }
+
+        /// <summary>
+        /// Obtém acaoQueixa com o código especificiado
+        /// </summary>
+        /// <param name="idAcaoQueixa"></param>
+        /// <returns></returns>
+        public AcaoQueixaModel Obter(int idAcaoQueixa)
+        {
+            return GetQuery().Where(acaoQueixa => acaoQueixa.IdAcaoQueixa == idAcaoQueixa).ToList().ElementAtOrDefault(0);
+        }
+
+        /// <summary>
+        /// Obtém acaoQueixa que iniciam com o descricaoAcao
+        /// </summary>
+        /// <param name="descricaoAcao"></param>
+        /// <returns></returns>
+        public IEnumerable<AcaoQueixaModel> ObterPorNome(string descricaoAcao)
+        {
+            return GetQuery().Where(acaoQueixa => acaoQueixa.DescricaoAcao.StartsWith(descricaoAcao)).ToList();
+        }
+
+        /// <summary>
+        /// Atribui dados da classe de modelo para classe entity de persistência
+        /// </summary>
+        /// <param name="acaoQueixa"></param>
+        /// <param name="_acaoQueixaE"></param>
+        private static void Atribuir(AcaoQueixaModel acaoQueixa, AcaoQueixaE _acaoQueixaE)
+        {
+            _acaoQueixaE.DescricaoAcao = acaoQueixa.DescricaoAcao;
+        }
+    }
+}
---
+++ trunk/Codigo/PacienteVirtual - Update base pelo VS2010/PacienteVirtual/Negocio/Consulta/GerenciadorConsultaVariavel.cs
@@ -0,0 +1,174 @@
+﻿using Persistence;
+using PacienteVirtual.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PacienteVirtual.Models.Negocio
+{
+    public class GerenciadorConsultaVariavel
+    {
+        private static GerenciadorConsultaVariavel gConsultaFixo;
+
+        private GerenciadorConsultaVariavel() { }
+
+        public static GerenciadorConsultaVariavel GetInstance()
+        {
+            if (gConsultaFixo == null)
+            {
+                gConsultaFixo = new GerenciadorConsultaVariavel();
+            }
+            return gConsultaFixo;
+        }
+
+        /// <summary>
+        /// Insere dados do consultaVariavel
+        /// </summary>
+        /// <param name="consultaVariavel"></param>
+        /// <returns></returns>
+        public long Inserir(ConsultaVariavelModel consultaVariavel)
+        {
+            var repConsultaVariavel = new RepositorioGenerico<ConsultaVariavelE>();
+            ConsultaVariavelE _consultaVariavelE = new ConsultaVariavelE();
+            try
+            {
+                Atribuir(consultaVariavel, _consultaVariavelE);
+
+                repConsultaVariavel.Inserir(_consultaVariavelE);
+                repConsultaVariavel.SaveChanges();
+
+                return _consultaVariavelE.IdConsultaVariavel;
+            }
+            catch (Exception e)
+            {
+                throw new DadosException("ConsultaVariavel", e.Message, e);
+            }
+        }
+
+        /// <summary>
+        /// Atualiza dados do consultaVariavel
+        /// </summary>
+        /// <param name="consultaVariavel"></param>
+        public void Atualizar(ConsultaVariavelModel consultaVariavel)
+        {
+            try
+            {
+                var repConsultaVariavel = new RepositorioGenerico<ConsultaVariavelE>();
+                ConsultaVariavelE _consultaVariavelE = repConsultaVariavel.ObterEntidade(cv => cv.IdConsultaVariavel == consultaVariavel.IdConsultaVariavel);
+                Atribuir(consultaVariavel, _consultaVariavelE);
+
+                repConsultaVariavel.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new DadosException("ConsultaVariavel", e.Message, e);
+            }
+        }
+
+        /// <summary>
+        /// Remove dados do consultaVariavel
+        /// </summary>
+        /// <param name="codDisciplina"></param>
+        public void Remover(long idConsultaVariavel)
+        {
+            //if (idConsultaVariavel == 1)
+            //    throw new NegocioException("A consultaVariavel não pode ser removido.");
+            try
+            {
+                var repConsultaVariavel = new RepositorioGenerico<ConsultaVariavelE>();
+                repConsultaVariavel.Remover(cv => cv.IdConsultaVariavel == idConsultaVariavel);
+                repConsultaVariavel.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new DadosException("ConsultaVariavel", e.Message, e);
+            }
+        }
+
+        /// <summary>
+        /// Consulta para retornar dados da entidade
+        /// </summary>
+        /// <returns></returns>
+        private IQueryable<ConsultaVariavelModel> GetQuery()
+        {
+            var repConsultaVariavel = new RepositorioGenerico<ConsultaVariavelE>();
+            var pvEntities = (pvEntities)repConsultaVariavel.ObterContexto();
+            var query = from tb_consulta_variavel in pvEntities.tb_consulta_variavel
+                        //join tb_pessoa in pvEntities.tb_pessoa
+                        //on tb_consulta_variavel.IdPessoa equals tb_pessoa.IdPessoa
+
+                        select new ConsultaVariavelModel
+                        {
+                            IdConsultaVariavel = tb_consulta_variavel.IdConsultaVariavel,
+                            IdConsultaFixo = tb_consulta_variavel.IdConsultaFixo,
+                            IdTurma = tb_consulta_variavel.IdTurma,
+                            IdPessoa = tb_consulta_variavel.IdPessoa,
+                            IdRelato = tb_consulta_variavel.IdRelato,
+                            IdRazaoEncontro = tb_consulta_variavel.IdRazaoEncontro,
+                            DataPreenchimento = (DateTime)tb_consulta_variavel.DataPreenchimento,
+                            Lembretes = tb_consulta_variavel.Lembretes,
+                            ComentariosTutor = tb_consulta_variavel.ComentariosTutor,
+
+                        };
+            return query;
+        }
+
+        /// <summary>
+        /// Obtém todos os consultaVariavel cadastrados
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<ConsultaVariavelModel> ObterTodos()
+        {
+            return GetQuery().ToList();
+        }
+
+        /// <summary>
+        /// Obtém consultaVariavel com o código especificiado
+        /// </summary>
+        /// <returns></returns>
+        public ConsultaVariavelModel Obter(long idConsultaVariavel)
+        {
+            return GetQuery().Where(consultaVariavel => consultaVariavel.IdConsultaVariavel == idConsultaVariavel).ToList().ElementAtOrDefault(0);
+        }
+
+
+        /// <summary>
+        /// Obtém consultaVariavel com o código da Turma, Pessoa e Relato especificiado
+        /// </summary>
+        /// <returns></returns>
+        public ConsultaVariavelModel Obter(int idTurma, int idPessoa, int idRelato)
+        {
+            return GetQuery().Where(consultaVariavel => consultaVariavel.IdTurma == idTurma
+                && consultaVariavel.IdPessoa == idPessoa && consultaVariavel.IdRelato == idRelato).ToList().ElementAtOrDefault(0);
+        }
+
+        /// <summary>
+        /// Obtém disciplinas que iniciam com o nome
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <returns></returns>
+        // public IEnumerable<ConsultaVariavelModel> ObterPorNome(int IdPaciente)
+        //{
+        //    return GetQuery().Where(consultaVariavel => consultaVariavel.IdPessoa == IdPaciente).ToList();
+        //}
+
+        /// <summary>
+        /// Atribui dados da classe de modelo para classe entity de persistência
+        /// </summary>
+        /// <param name="consultaVariavel"></param>
+        /// <param name="_consultaVariavelE"></param>
+        private static void Atribuir(ConsultaVariavelModel consultaVariavel, ConsultaVariavelE _consultaVariavelE)
+        {
+            _consultaVariavelE.IdConsultaVariavel = consultaVariavel.IdConsultaVariavel;
+            _consultaVariavelE.IdConsultaFixo = consultaVariavel.IdConsultaFixo;
+            _consultaVariavelE.IdTurma = consultaVariavel.IdTurma;
+            _consultaVariavelE.IdPessoa = consultaVariavel.IdPessoa;
+            _consultaVariavelE.IdRelato = consultaVariavel.IdRelato;
+            _consultaVariavelE.IdRazaoEncontro = consultaVariavel.IdRazaoEncontro;
+            _consultaVariavelE.DataPreenchimento = (DateTime)consultaVariavel.DataPreenchimento;
+            _consultaVariavelE.Lembretes = consultaVariavel.Lembretes;
+            _consultaVariavelE.ComentariosTutor = consultaVariavel.ComentariosTutor;
+        }
+    }
+}
