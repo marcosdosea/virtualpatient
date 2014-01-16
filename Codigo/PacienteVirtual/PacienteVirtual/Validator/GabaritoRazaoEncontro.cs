281b74e0d95c63b0742145ffc0d62487f0a943ce
marcosdosea@gmail.com <marcosdosea@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2014-01-16 00:53:12 +0000

    Atualiações na base de dados para incluir especialidade na carta.

---
+++ Codigo/PacienteVirtual/PacienteVirtual/Validator/GabaritoRazaoEncontro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using PacienteVirtual.Controllers;
+
+namespace PacienteVirtual.Validator
+{
+    public class GabaritoRazaoEncontro : ValidationAttribute
+    {
+        public GabaritoRazaoEncontro(params string[] propertyNames)
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (SessionController.EmCorrecao)
+            {
+                if (!value.Equals(1)) // pega valor do gabarito disponível na sessão
+                    return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+            }
+            return null;
+        }
+    }
+}
