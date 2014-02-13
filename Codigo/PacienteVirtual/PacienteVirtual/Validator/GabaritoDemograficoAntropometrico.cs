f7fe30866b4b5768401e73be876c575b64c1e9db
marcosdosea@gmail.com <marcosdosea@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2014-02-13 03:07:46 +0000

    Correçãdo do flag na consulta para indicar que o formulário estã ou não em correção.

---
+++ Codigo/PacienteVirtual/PacienteVirtual/Validator/GabaritoDemograficoAntropometrico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using PacienteVirtual.Controllers;
+using PacienteVirtual.Models;
+
+namespace PacienteVirtual.Validator 
+{
+    public class GabaritoDemograficoAntropometrico : ValidationAttribute
+    {
+        public GabaritoDemograficoAntropometrico(params string[] propertyNames)
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (SessionController.EmCorrecao)
+            {
+                DemograficosAntropometricosModel demograficosAntropometricosGabarito = SessionController.DemograficosAntropometricos; //SessionController.ConsultaGabarito;
+                
+                var model = (DemograficosAntropometricosModel)validationContext.ObjectInstance;
+                if (model.Nome.Equals(demograficosAntropometricosGabarito.Nome)) // aqui iria comparar com o gabarito
+                {
+                    return new ValidationResult("Nome difere do gabarito. O valor correto seria " + demograficosAntropometricosGabarito.Nome + ".", new List<string> { "nome" });
+                }
+                
+                return ValidationResult.Success;
+
+            }
+            return null;
+        }
+    }
+}
