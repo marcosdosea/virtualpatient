4b5e6be7a08c04a3639895f0f96ea13158607622
andres.s.menendez@gmail.com <andres.s.menendez@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2014-04-23 21:25:19 +0000

    Feito a internacionalização dos Enums com o EnumDropDownListFor.cs

--- Codigo/PacienteVirtual/PacienteVirtual/Helpers/EnumDropDownListFor.cs
+++ Codigo/PacienteVirtual/PacienteVirtual/Helpers/EnumDropDownListFor.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
-using System.Web.Mvc.Html; 
+using System.Web.Mvc.Html;
+using Resources;
+using System.ComponentModel;
+using System.Resources;
+using System.Reflection;
+using PacienteVirtual.Models;
+using System.Web;
+
 namespace PacienteVirtual.Helpers
 {
     public static class Extension
@@ -13,17 +20,40 @@ namespace PacienteVirtual.Helpers
                     Expression<Func<TModel, TProperty>> expression,
                     TEnum selectedValue)
         {
+            
+
             IEnumerable<TEnum> values = Enum.GetValues(typeof(TEnum))
                                         .Cast<TEnum>();
+
             IEnumerable<SelectListItem> items = from value in values
                                                 select new SelectListItem()
                                                 {
                                                     Text = value.ToString(),
-                                                    //Fazer a conversão de "_" pelo espaço em branco
                                                     Value = value.ToString(),
                                                     Selected = (value.Equals(selectedValue))
                                                 };
-            return SelectExtensions.DropDownListFor(htmlHelper, expression, items);
+
+            LocalizarDisplayNomeAtributo[] listaItens = new LocalizarDisplayNomeAtributo[values.Count()];
+            LocalizarDisplayNomeAtributo na;
+            int i = 0;
+            foreach (var item in items)
+            {
+                na = new LocalizarDisplayNomeAtributo(item.Text);
+                na.NameResourceType = typeof(Mensagem);
+                listaItens[i] = na;
+                i++;
+            }
+
+            i = 0;
+            IEnumerable<SelectListItem> valores = from value in values
+                                                  select new SelectListItem()
+                                                  {
+                                                      Text = listaItens[i++].DisplayName,
+                                                      Value = value.ToString(),
+                                                      Selected = (value.Equals(selectedValue))
+                                                  };
+
+            return SelectExtensions.DropDownListFor(htmlHelper, expression, valores);
         }
     }
 }
