00ddbea18d0e20c17c78a01ae51850627ee80354
marcosdosea@gmail.com <marcosdosea@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2014-11-15 14:01:30 +0000

    conversão dos scripts para utf-8

--- Documentos/nanda/dominio13/Caracteristicas_Dominio13.sql
+++ Documentos/nanda/dominio13/Caracteristicas_Dominio13.sql
@@ -1,8 +1,8 @@
-INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Afeto embotado");
-INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Atraso em desempenhar habilidades tÌpicas do grupo et·rio");
-INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Crescimento fÌsico alterado");
-INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "DesatenÁ„o");
-INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Dificuldade em desempenhar habilidades tÌpicas do grupo et·rio");
-INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Incapacidade de desempenhar atividades de auto controle apropriadas ‡ idade");
-INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Incapacidade de realizar atividades de autocuidado apropriadas ‡ idade");
-INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Tempo de reaÁ„o diminuÌdo");
+ÔªøINSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Afeto embotado");
+INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Atraso em desempenhar habilidades t√≠picas do grupo et√°rio");
+INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Crescimento f√≠sico alterado");
+INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Desaten√ß√£o");
+INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Dificuldade em desempenhar habilidades t√≠picas do grupo et√°rio");
+INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Incapacidade de desempenhar atividades de auto controle apropriadas √† idade");
+INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Incapacidade de realizar atividades de autocuidado apropriadas √† idade");
+INSERT INTO `pv1`.`tb_diagnostico_caracteristica` (`IdDiagnostico`, `DescricaoCaracteristica`) VALUES (216, "Tempo de rea√ß√£o diminu√≠do");
