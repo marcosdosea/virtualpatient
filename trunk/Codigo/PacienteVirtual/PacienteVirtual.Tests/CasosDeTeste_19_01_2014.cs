bd7443976261eff2bd0eda34849365d6cde76b2d
clevertonmaggot@gmail.com <clevertonmaggot@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2015-01-20 19:48:35 +0000

    Atualização casos de teste (CasosDeTeste_19_01_2014)

--- trunk/Codigo/PacienteVirtual/PacienteVirtual.Tests/CasosDeTeste_19_01_2014.cs
+++ trunk/Codigo/PacienteVirtual/PacienteVirtual.Tests/CasosDeTeste_19_01_2014.cs
@@ -64,7 +64,33 @@ namespace PacienteVirtual.Tests
 
         public void PV11_AtribuirRelatoClinicoParaAlunoDaTurma()
         {
-            // TODO
+            int pessoa = 10;
+            int turma = 6;
+            long consultaFixa = 0;
+            int relato = 66;
+            ConsultaFixoModel cfm = new ConsultaFixoModel();
+            consultaFixa = GerenciadorConsultaFixo.GetInstance().Inserir(cfm);
+            Assert.IsTrue(consultaFixa >= 0);
+
+            TurmaPessoaRelatoModel tprm = new TurmaPessoaRelatoModel();
+            tprm.IdConsultaFixo = consultaFixa;
+            tprm.IdPessoa = pessoa;
+            tprm.IdTurma = turma;
+            tprm.IdRelato = relato;
+            GerenciadorTurmaPessoaRelato.GetInstance().Inserir(tprm);
+            TurmaPessoaRelatoModel tprmArmazenada = GerenciadorTurmaPessoaRelato.GetInstance().Obter(pessoa, turma, relato, consultaFixa);
+            Assert.IsNotNull(tprmArmazenada);
+
+            ConsultaVariavelModel cvm = new ConsultaVariavelModel();
+            cvm.IdConsultaFixo = consultaFixa;
+            cvm.IdEstadoConsulta = Global.AguardandoPreenchimento;
+            cvm.IdPessoa = pessoa;
+            cvm.IdTurma = turma;
+            cvm.IdRelato = relato;
+            cvm.IdRazaoEncontro = Global.IdRazaoEncontro;
+            long idConsultaVariavel = GerenciadorConsultaVariavel.GetInstance().Inserir(cvm);
+            Assert.IsTrue(idConsultaVariavel >= 0);
+
         }
 
         public void PV12_AtualizarDadosIdentificacaoValido()
