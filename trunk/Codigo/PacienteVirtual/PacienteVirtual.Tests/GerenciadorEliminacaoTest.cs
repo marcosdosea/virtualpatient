ea335356e351c1da4f33ff7a200e00b36022dfab
clevertonmaggot@gmail.com <clevertonmaggot@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2015-02-02 21:20:59 +0000

    Casos de teste de interação dia 02/02/2015; )

---
+++ trunk/Codigo/PacienteVirtual/PacienteVirtual.Tests/GerenciadorEliminacaoTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacienteVirtual.Negocio;
+using PacienteVirtual.Models;
+
+namespace PacienteVirtual.Tests
+{
+    /// <summary>
+    /// Summary description for GerenciadorEliminacaoTest
+    /// </summary>
+    [TestClass]
+    public class GerenciadorEliminacaoTest
+    {
+        [TestMethod()]
+        public void PV32_AtualizarNecessidadesPsicobiologicasEliminacaoValida()
+        {
+            long idConsultaVariavel = 100;
+            GerenciadorEliminacao gerenciadorEliminacao = GerenciadorEliminacao.GetInstance();
+            EliminacaoModel eliminacao = gerenciadorEliminacao.Obter(idConsultaVariavel);
+            Assert.IsNotNull(eliminacao);
+            eliminacao.Cistostomia = true;
+            eliminacao.ColoracaoUrinaria = ListaColoracaoUrinaria.LimpidaClara;
+            eliminacao.CondicaoContinenciaUrinaria = ListaCondicaoContinenciaUrinaria.RetencaoUrinaria;
+            eliminacao.DebitoUrinario = ListaDebitoUrinario.Normal;
+            eliminacao.Disuria = false;
+            eliminacao.EsforcoEvacuar = true;
+            eliminacao.EstomasCirurgicos = ListaEstomasCirurgicos.NaoSeAplica;
+            eliminacao.EvacuacoesDia = 4;
+            eliminacao.Flatos = false;
+            eliminacao.FormasAbdomem = ListaFormasAbdomem.Retraido;
+            eliminacao.Hemorroidas = false;
+            eliminacao.IncontinenciaFecal = true;
+            eliminacao.PalpacaoAbodminal = ListaPalpacaoAbodminal.Tenso;
+            eliminacao.PruridoAnal = false;
+            eliminacao.SangramentoRetal = false;
+            eliminacao.SonsIntestinais = ListaSonsIntestinais.Diminuidos;
+            eliminacao.SVD = false;
+            eliminacao.SVDInstalada = Convert.ToDateTime("11/11/2011");
+            eliminacao.Tempo = "12:00";
+            eliminacao.TiposFezes = ListaTiposFezes.Constipacao;
+            eliminacao.TipoSonsPercussao = ListaTipoSonsPercussao.SomMacico;
+            eliminacao.UltimaEvacuacao = Convert.ToDateTime("02/02/2015");
+
+            gerenciadorEliminacao.Atualizar(eliminacao);
+
+            EliminacaoModel eliminacaoAtualizada = gerenciadorEliminacao.Obter(idConsultaVariavel);
+            Assert.Equals(eliminacaoAtualizada.Cistostomia, true);
+            Assert.Equals(eliminacaoAtualizada.ColoracaoUrinaria, ListaColoracaoUrinaria.LimpidaClara);
+            Assert.Equals(eliminacaoAtualizada.CondicaoContinenciaUrinaria, ListaCondicaoContinenciaUrinaria.RetencaoUrinaria);
+            Assert.Equals(eliminacaoAtualizada.DebitoUrinario, ListaDebitoUrinario.Normal);
+            Assert.Equals(eliminacaoAtualizada.Disuria, false);
+            Assert.Equals(eliminacaoAtualizada.EsforcoEvacuar, true);
+            Assert.Equals(eliminacaoAtualizada.EstomasCirurgicos, ListaEstomasCirurgicos.NaoSeAplica);
+            Assert.Equals(eliminacaoAtualizada.EvacuacoesDia, 4);
+            Assert.Equals(eliminacaoAtualizada.Flatos, false);
+            Assert.Equals(eliminacaoAtualizada.FormasAbdomem, ListaFormasAbdomem.Retraido);
+            Assert.Equals(eliminacaoAtualizada.Hemorroidas, false);
+            Assert.Equals(eliminacaoAtualizada.IdConsultaVariavel, idConsultaVariavel);
+            Assert.Equals(eliminacaoAtualizada.IncontinenciaFecal, true);
+            Assert.Equals(eliminacaoAtualizada.PalpacaoAbodminal, ListaPalpacaoAbodminal.Tenso);
+            Assert.Equals(eliminacaoAtualizada.PruridoAnal, false);
+            Assert.Equals(eliminacaoAtualizada.SangramentoRetal, false);
+            Assert.Equals(eliminacaoAtualizada.SonsIntestinais, ListaSonsIntestinais.Diminuidos);
+            Assert.Equals(eliminacaoAtualizada.SVD, false);
+            Assert.Equals(eliminacaoAtualizada.SVDInstalada, Convert.ToDateTime("11/11/2011"));
+            Assert.Equals(eliminacaoAtualizada.Tempo, "12:00");
+            Assert.Equals(eliminacaoAtualizada.TiposFezes, ListaTiposFezes.Constipacao);
+            Assert.Equals(eliminacaoAtualizada.TipoSonsPercussao, ListaTipoSonsPercussao.SomMacico);
+            Assert.Equals(eliminacaoAtualizada.UltimaEvacuacao, Convert.ToDateTime("02/02/2015"));
+        }
+
+        [TestMethod()]
+        public void PV33_AtualizarNecessidadesPsicobiologicasEliminacaoInvalida()
+        {
+            long idConsultaVariavel = 100;
+            GerenciadorEliminacao gerenciadorEliminacao = GerenciadorEliminacao.GetInstance();
+            EliminacaoModel eliminacao = gerenciadorEliminacao.Obter(idConsultaVariavel);
+            Assert.IsNotNull(eliminacao);
+            eliminacao.IdConsultaVariavel = -1;
+            eliminacao.Cistostomia = true;
+            eliminacao.ColoracaoUrinaria = ListaColoracaoUrinaria.LimpidaClara;
+            eliminacao.CondicaoContinenciaUrinaria = ListaCondicaoContinenciaUrinaria.RetencaoUrinaria;
+            eliminacao.EsforcoEvacuar = true;
+            eliminacao.EstomasCirurgicos = ListaEstomasCirurgicos.NaoSeAplica;
+            eliminacao.EvacuacoesDia = 4;
+            eliminacao.Flatos = false;
+            eliminacao.FormasAbdomem = ListaFormasAbdomem.Retraido;
+            eliminacao.Hemorroidas = false;
+            eliminacao.IncontinenciaFecal = true;
+            eliminacao.PalpacaoAbodminal = ListaPalpacaoAbodminal.Tenso;
+            eliminacao.SonsIntestinais = ListaSonsIntestinais.Diminuidos;
+
+            try
+            {
+                gerenciadorEliminacao.Atualizar(eliminacao);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(NegocioException));
+            }
+
+            EliminacaoModel eliminacaoAtualizada = gerenciadorEliminacao.Obter(idConsultaVariavel);
+            Assert.Equals(eliminacaoAtualizada.Cistostomia, false);
+            Assert.Equals(eliminacaoAtualizada.ColoracaoUrinaria, ListaColoracaoUrinaria.LimpidaClara);
+            Assert.Equals(eliminacaoAtualizada.CondicaoContinenciaUrinaria, ListaCondicaoContinenciaUrinaria.IncontinenciaUrinaria);
+            Assert.Equals(eliminacaoAtualizada.DebitoUrinario, ListaDebitoUrinario.NaoSeAplica);
+            Assert.Equals(eliminacaoAtualizada.Disuria, false);
+            Assert.Equals(eliminacaoAtualizada.EsforcoEvacuar, false);
+            Assert.Equals(eliminacaoAtualizada.EstomasCirurgicos, ListaEstomasCirurgicos.NaoSeAplica);
+            Assert.Equals(eliminacaoAtualizada.EvacuacoesDia, 0);
+            Assert.Equals(eliminacaoAtualizada.Flatos, false);
+            Assert.Equals(eliminacaoAtualizada.FormasAbdomem, ListaFormasAbdomem.Plano);
+            Assert.Equals(eliminacaoAtualizada.Hemorroidas, false);
+            Assert.Equals(eliminacaoAtualizada.IdConsultaVariavel, 100);
+            Assert.Equals(eliminacaoAtualizada.IncontinenciaFecal, false);
+            Assert.Equals(eliminacaoAtualizada.PalpacaoAbodminal, ListaPalpacaoAbodminal.Normotenso);
+            Assert.Equals(eliminacaoAtualizada.PruridoAnal, false);
+            Assert.Equals(eliminacaoAtualizada.SangramentoRetal, false);
+            Assert.Equals(eliminacaoAtualizada.SonsIntestinais, ListaSonsIntestinais.Presentes);
+            Assert.Equals(eliminacaoAtualizada.SVD, false);
+            Assert.Equals(eliminacaoAtualizada.SVDInstalada, Convert.ToDateTime("15/07/2014"));
+            Assert.Equals(eliminacaoAtualizada.Tempo, "");
+            Assert.Equals(eliminacaoAtualizada.TiposFezes, ListaTiposFezes.FezesFormadas);
+            Assert.Equals(eliminacaoAtualizada.TipoSonsPercussao, ListaTipoSonsPercussao.SomMacico);
+            Assert.Equals(eliminacaoAtualizada.UltimaEvacuacao, Convert.ToDateTime("15/07/2014"));
+        }
+    }
+}
---
+++ trunk/Codigo/PacienteVirtual/PacienteVirtual.Tests/GerenciadorIntegridadeTecidualTest.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacienteVirtual.Negocio;
+using PacienteVirtual.Models;
+
+namespace PacienteVirtual.Tests
+{
+    /// <summary>
+    /// Summary description for GerenciadorIntegridadeTecidualTest
+    /// </summary>
+    [TestClass]
+    public class GerenciadorIntegridadeTecidualTest
+    {
+        [TestMethod()]
+        public void PV30_AtualizarNecessidadesPsicobiologicasIntegridadeTecidualValida()
+        {
+            long idConsultaVariavel = 100;
+            GerenciadorIntegridadeTecidual gerenciadorIntegridadeTecidual = GerenciadorIntegridadeTecidual.GetInstance();
+            IntegridadeTecidualModel integridadeTecidual = gerenciadorIntegridadeTecidual.Obter(idConsultaVariavel);
+            Assert.IsNotNull(integridadeTecidual);
+            integridadeTecidual.Descamacao = true;
+            integridadeTecidual.Descorada = false;
+            integridadeTecidual.Equimose = true;
+            integridadeTecidual.Eritema = true;
+            integridadeTecidual.Escoriacao = false;
+            integridadeTecidual.EstadoHidratacao = ListaEstadoHidratacao.Hidratada;
+            integridadeTecidual.FeridaCirurgica = true;
+            integridadeTecidual.FeridaCirurgicaLocal = "Barriga";
+            integridadeTecidual.Hematoma = false;
+            integridadeTecidual.Hiperemia = false;
+            integridadeTecidual.LesaoAspecto = "Local muito roxo";
+            integridadeTecidual.Letericia = true;
+            integridadeTecidual.LocalAlteracaoPele = "Perna";
+            integridadeTecidual.Nodulo = true;
+            integridadeTecidual.Pediabetico = false;
+            integridadeTecidual.PerdaSensibilidade = false;
+            integridadeTecidual.Purido = false;
+            integridadeTecidual.Queimadura = false;
+            integridadeTecidual.QueimaduraGrau = "";
+            integridadeTecidual.QueimaduraLocal = "";
+            integridadeTecidual.Rubor = true;
+            integridadeTecidual.Turgor = ListaTurgor.Diminuido;
+            integridadeTecidual.UlceraPressao = true;
+            integridadeTecidual.UlceraPressaoEstagio = "Avançado";
+            integridadeTecidual.UlceraPressaoLocal = "Barriga";
+
+            gerenciadorIntegridadeTecidual.Atualizar(integridadeTecidual);
+
+            IntegridadeTecidualModel intTectAtualizada = gerenciadorIntegridadeTecidual.Obter(idConsultaVariavel);
+            Assert.Equals(intTectAtualizada.Descamacao, true);
+            Assert.Equals(intTectAtualizada.Descorada, false);
+            Assert.Equals(intTectAtualizada.Equimose, true);
+            Assert.Equals(intTectAtualizada.Eritema, true);
+            Assert.Equals(intTectAtualizada.Escoriacao, false);
+            Assert.Equals(intTectAtualizada.EstadoHidratacao, ListaEstadoHidratacao.Hidratada);
+            Assert.Equals(intTectAtualizada.FeridaCirurgica, true);
+            Assert.Equals(intTectAtualizada.FeridaCirurgicaLocal, "Barriga");
+            Assert.Equals(intTectAtualizada.Hematoma, false);
+            Assert.Equals(intTectAtualizada.Hiperemia, false);
+            Assert.Equals(intTectAtualizada.IdConsultaVariavel, idConsultaVariavel);
+            Assert.Equals(intTectAtualizada.LesaoAspecto, "Local muito roxo");
+            Assert.Equals(intTectAtualizada.Letericia, true);
+            Assert.Equals(intTectAtualizada.LocalAlteracaoPele, "Perna");
+            Assert.Equals(intTectAtualizada.Nodulo, true);
+            Assert.Equals(intTectAtualizada.Pediabetico, false);
+            Assert.Equals(intTectAtualizada.PerdaSensibilidade, false);
+            Assert.Equals(intTectAtualizada.Purido, false);
+            Assert.Equals(intTectAtualizada.Queimadura, false);
+            Assert.Equals(intTectAtualizada.QueimaduraGrau, "");
+            Assert.Equals(intTectAtualizada.QueimaduraLocal, "");
+            Assert.Equals(intTectAtualizada.Rubor, true);
+            Assert.Equals(intTectAtualizada.Turgor, ListaTurgor.Diminuido);
+            Assert.Equals(intTectAtualizada.UlceraPressao, true);
+            Assert.Equals(intTectAtualizada.UlceraPressaoEstagio, "Avançado");
+            Assert.Equals(intTectAtualizada.UlceraPressaoLocal, "Barriga");
+        }
+
+        [TestMethod()]
+        public void PV31_AtualizarNecessidadesPsicobiologicasIntegridadeTecidualInvalida()
+        {
+            long idConsultaVariavel = 100;
+            GerenciadorIntegridadeTecidual gerenciadorIntegridadeTecidual = GerenciadorIntegridadeTecidual.GetInstance();
+            IntegridadeTecidualModel integridadeTecidual = gerenciadorIntegridadeTecidual.Obter(idConsultaVariavel);
+            Assert.IsNotNull(integridadeTecidual);
+            integridadeTecidual.IdConsultaVariavel = -1;
+            integridadeTecidual.Descamacao = true;
+            integridadeTecidual.Equimose = true;
+            integridadeTecidual.Eritema = true;
+            integridadeTecidual.FeridaCirurgica = true;
+            integridadeTecidual.Letericia = true;
+            integridadeTecidual.LocalAlteracaoPele = "Perna";
+            integridadeTecidual.Nodulo = true;
+            integridadeTecidual.Pediabetico = false;
+            integridadeTecidual.PerdaSensibilidade = false;
+            integridadeTecidual.Purido = false;
+            integridadeTecidual.UlceraPressao = true;
+            integridadeTecidual.UlceraPressaoEstagio = "Avançado";
+            integridadeTecidual.UlceraPressaoLocal = "Barriga";
+
+            try
+            {
+                gerenciadorIntegridadeTecidual.Atualizar(integridadeTecidual);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(NegocioException));
+            }
+            
+            IntegridadeTecidualModel intTectAtualizada = gerenciadorIntegridadeTecidual.Obter(idConsultaVariavel);
+            Assert.Equals(intTectAtualizada.Descamacao, false);
+            Assert.Equals(intTectAtualizada.Descorada, false);
+            Assert.Equals(intTectAtualizada.Equimose, false);
+            Assert.Equals(intTectAtualizada.Eritema, false);
+            Assert.Equals(intTectAtualizada.Escoriacao, false);
+            Assert.Equals(intTectAtualizada.EstadoHidratacao, ListaEstadoHidratacao.Hidratada);
+            Assert.Equals(intTectAtualizada.FeridaCirurgica, false);
+            Assert.Equals(intTectAtualizada.FeridaCirurgicaLocal, "");
+            Assert.Equals(intTectAtualizada.Hematoma, false);
+            Assert.Equals(intTectAtualizada.Hiperemia, false);
+            Assert.Equals(intTectAtualizada.IdConsultaVariavel, idConsultaVariavel);
+            Assert.Equals(intTectAtualizada.LesaoAspecto, "");
+            Assert.Equals(intTectAtualizada.Letericia, false);
+            Assert.Equals(intTectAtualizada.LocalAlteracaoPele, "");
+            Assert.Equals(intTectAtualizada.Nodulo, false);
+            Assert.Equals(intTectAtualizada.Pediabetico, false);
+            Assert.Equals(intTectAtualizada.PerdaSensibilidade, false);
+            Assert.Equals(intTectAtualizada.Purido, false);
+            Assert.Equals(intTectAtualizada.Queimadura, false);
+            Assert.Equals(intTectAtualizada.QueimaduraGrau, "");
+            Assert.Equals(intTectAtualizada.QueimaduraLocal, "");
+            Assert.Equals(intTectAtualizada.Rubor, false);
+            Assert.Equals(intTectAtualizada.Turgor, ListaTurgor.Presente);
+            Assert.Equals(intTectAtualizada.UlceraPressao, false);
+            Assert.Equals(intTectAtualizada.UlceraPressaoEstagio, "");
+            Assert.Equals(intTectAtualizada.UlceraPressaoLocal, "");
+        }
+    }
+}
