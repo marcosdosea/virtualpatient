648e7c5e2ced30fe2a9362b5ed27096ee2a65694
clevertonmaggot@gmail.com <clevertonmaggot@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2015-02-03 17:09:09 +0000

    Casos de teste de interação dia 09/02/2015;

---
+++ Codigo/PacienteVirtual/PacienteVirtual.Tests/GerenciadorOutrasNecessidadesTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacienteVirtual.Negocio;
+using PacienteVirtual.Models;
+
+namespace PacienteVirtual.Tests
+{
+    /// <summary>
+    /// Summary description for GerenciadorOutrasNecessidadesTest
+    /// </summary>
+    [TestClass]
+    public class GerenciadorOutrasNecessidadesTest
+    {
+        [TestMethod()]
+        public void PV42AtualizarOutrasNecessidadesValida()
+        {
+            long idConsultaVariavel = 100;
+            GerenciadorOutrasNecessidades gerenciadorOutrasNecessidades = GerenciadorOutrasNecessidades.GetInstance();
+            OutrasNecessidadesModel outrasNecessidades = gerenciadorOutrasNecessidades.Obter(idConsultaVariavel);
+            Assert.IsNotNull(outrasNecessidades);
+            outrasNecessidades.DescreverTipoTempoQtde = "Cigarro, 3anos, 2 por dia";
+            outrasNecessidades.DescricaoEstilista = "";
+            outrasNecessidades.DescricaoGrauDependencia = "Muito Dependente";
+            outrasNecessidades.DescricaoTabagista = "Fumante ativo";
+            outrasNecessidades.Estilista = false;
+            outrasNecessidades.FrequenciaDrogas = "Toda semana";
+            outrasNecessidades.GrauDependencia = ListaGrauDependencia.ParcialmenteDependente;
+            outrasNecessidades.QuantidadeDrogas = "Pouca";
+            outrasNecessidades.Tabagista = true;
+            outrasNecessidades.TempoDrogas = "Dois anos";
+            outrasNecessidades.TipoDrogas = "Cigarro e maconha";
+            outrasNecessidades.UsoDrogasIlicitas = true;
+
+            gerenciadorOutrasNecessidades.Atualizar(outrasNecessidades);
+
+            OutrasNecessidadesModel outrasNecessidadesAtualizada = gerenciadorOutrasNecessidades.Obter(idConsultaVariavel);
+            Assert.Equals(outrasNecessidadesAtualizada.DescreverTipoTempoQtde, "Cigarro, 3anos, 2 por dia");
+            Assert.Equals(outrasNecessidadesAtualizada.DescricaoEstilista, "");
+            Assert.Equals(outrasNecessidadesAtualizada.DescricaoGrauDependencia, "Muito Dependente");
+            Assert.Equals(outrasNecessidadesAtualizada.DescricaoTabagista, "Fumante ativo");
+            Assert.Equals(outrasNecessidadesAtualizada.Estilista, false);
+            Assert.Equals(outrasNecessidadesAtualizada.FrequenciaDrogas, "Toda semana");
+            Assert.Equals(outrasNecessidadesAtualizada.GrauDependencia, ListaGrauDependencia.ParcialmenteDependente);
+            Assert.Equals(outrasNecessidadesAtualizada.IdConsultaVariavel, idConsultaVariavel);
+            Assert.Equals(outrasNecessidadesAtualizada.QuantidadeDrogas, "Pouca");
+            Assert.Equals(outrasNecessidadesAtualizada.Tabagista, true);
+            Assert.Equals(outrasNecessidadesAtualizada.TempoDrogas, "Dois anos");
+            Assert.Equals(outrasNecessidadesAtualizada.TipoDrogas, "Cigarro e maconha");
+            Assert.Equals(outrasNecessidadesAtualizada.UsoDrogasIlicitas, true);
+        }
+
+        [TestMethod()]
+        public void PV43AtualizarOutrasNecessidadesInvalida()
+        {
+            long idConsultaVariavel = 100;
+            GerenciadorOutrasNecessidades gerenciadorOutrasNecessidades = GerenciadorOutrasNecessidades.GetInstance();
+            OutrasNecessidadesModel outrasNecessidades = gerenciadorOutrasNecessidades.Obter(idConsultaVariavel);
+            Assert.IsNotNull(outrasNecessidades);
+            outrasNecessidades.IdConsultaVariavel = -1;
+            outrasNecessidades.DescreverTipoTempoQtde = "Cigarro, 3anos, 2 por dia";
+            outrasNecessidades.DescricaoEstilista = "";
+            outrasNecessidades.DescricaoGrauDependencia = "Muito Dependente";
+            outrasNecessidades.DescricaoTabagista = "Fumante ativo";
+            outrasNecessidades.Estilista = false;
+            outrasNecessidades.FrequenciaDrogas = "Toda semana";
+            outrasNecessidades.GrauDependencia = ListaGrauDependencia.ParcialmenteDependente;
+            outrasNecessidades.QuantidadeDrogas = "Pouca";
+            outrasNecessidades.Tabagista = true;
+            outrasNecessidades.TempoDrogas = "Dois anos";
+            outrasNecessidades.TipoDrogas = "Cigarro e maconha";
+            outrasNecessidades.UsoDrogasIlicitas = true;
+
+            try
+            {
+                gerenciadorOutrasNecessidades.Atualizar(outrasNecessidades);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(NegocioException));
+            }
+
+            OutrasNecessidadesModel outrasNecessidadesAtualizada = gerenciadorOutrasNecessidades.Obter(idConsultaVariavel);
+            Assert.Equals(outrasNecessidadesAtualizada.DescreverTipoTempoQtde, "");
+            Assert.Equals(outrasNecessidadesAtualizada.DescricaoEstilista, "");
+            Assert.Equals(outrasNecessidadesAtualizada.DescricaoGrauDependencia, "");
+            Assert.Equals(outrasNecessidadesAtualizada.DescricaoTabagista, "");
+            Assert.Equals(outrasNecessidadesAtualizada.Estilista, false);
+            Assert.Equals(outrasNecessidadesAtualizada.FrequenciaDrogas, "");
+            Assert.Equals(outrasNecessidadesAtualizada.GrauDependencia, ListaGrauDependencia.Independente);
+            Assert.Equals(outrasNecessidadesAtualizada.IdConsultaVariavel, idConsultaVariavel);
+            Assert.Equals(outrasNecessidadesAtualizada.QuantidadeDrogas, "");
+            Assert.Equals(outrasNecessidadesAtualizada.Tabagista, false);
+            Assert.Equals(outrasNecessidadesAtualizada.TempoDrogas, "");
+            Assert.Equals(outrasNecessidadesAtualizada.TipoDrogas, "");
+            Assert.Equals(outrasNecessidadesAtualizada.UsoDrogasIlicitas, false);
+        }
+    }
+}
---
+++ Codigo/PacienteVirtual/PacienteVirtual.Tests/GerenciadorPsicoEspiritualTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacienteVirtual.Negocio;
+using PacienteVirtual.Models;
+
+namespace PacienteVirtual.Tests
+{
+    /// <summary>
+    /// Summary description for GerenciadorPsicoEspiritualTest
+    /// </summary>
+    [TestClass]
+    public class GerenciadorPsicoEspiritualTest
+    {
+        [TestMethod()]
+        public void PV41AtualizarNecessidadesPsicoespirituaisInvalida()
+        {
+            long idConsultaVariavel = 100;
+            GerenciadorPsicoEspiritual gerenciadorPsicoEspiritual = GerenciadorPsicoEspiritual.GetInstance();
+            PsicoEspiritualModel psicoEspiritual = gerenciadorPsicoEspiritual.Obter(idConsultaVariavel);
+            Assert.IsNotNull(psicoEspiritual);
+            psicoEspiritual.IdConsultaVariavel = -1;
+            psicoEspiritual.Ansiedade = true;
+            psicoEspiritual.Apatico = false;
+            psicoEspiritual.BaixoAutoEstima = true;
+            psicoEspiritual.BuscaAssistenciaEspiritual = true;
+            psicoEspiritual.Choro = false;
+            psicoEspiritual.CrencaReligiosa = null;
+            psicoEspiritual.DisturbiosSono = false;
+            psicoEspiritual.EspecificaAssistenciaEspiritual = null;
+            psicoEspiritual.Estresse = true;
+            psicoEspiritual.PreocupacaoMorte = true;
+            psicoEspiritual.Raiva = true;
+
+            try
+            {
+                gerenciadorPsicoEspiritual.Atualizar(psicoEspiritual);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(NegocioException));
+            }
+
+            PsicoEspiritualModel psicoEspiritualAtualizado = gerenciadorPsicoEspiritual.Obter(idConsultaVariavel);
+            Assert.Equals(psicoEspiritualAtualizado.Ansiedade, false);
+            Assert.Equals(psicoEspiritualAtualizado.Apatico, false);
+            Assert.Equals(psicoEspiritualAtualizado.BaixoAutoEstima, false);
+            Assert.Equals(psicoEspiritualAtualizado.BuscaAssistenciaEspiritual, false);
+            Assert.Equals(psicoEspiritualAtualizado.Choro, false);
+            Assert.IsNull(psicoEspiritualAtualizado.CrencaReligiosa);
+            Assert.Equals(psicoEspiritualAtualizado.DisturbiosSono, false);
+            Assert.IsNull(psicoEspiritualAtualizado.EspecificaAssistenciaEspiritual);
+            Assert.Equals(psicoEspiritualAtualizado.Estresse, false);
+            Assert.Equals(psicoEspiritualAtualizado.HumorDeprimido, false);
+            Assert.Equals(psicoEspiritualAtualizado.IdConsultaVariavel, idConsultaVariavel);
+            Assert.Equals(psicoEspiritualAtualizado.Negacao, false);
+            Assert.Equals(psicoEspiritualAtualizado.PreocupacaoMorte, false);
+            Assert.Equals(psicoEspiritualAtualizado.Raiva, false);
+        }
+    }
+}
