547aa07b7b52a1d906aa1dd1bb5085664889a1dc
andres.s.menendez@gmail.com <andres.s.menendez@gmail.com@8fcffc36-12c5-ec34-5698-6f407c149cfc>
2015-02-28 11:57:12 +0000

    Adição dos testes de Sensorial, Sexualidade e PsicoEspiritual

--- trunk/Codigo/PacienteVirtual/PacienteVirtual.Tests/GerenciadorPsicoEspiritualTest.cs
+++ trunk/Codigo/PacienteVirtual/PacienteVirtual.Tests/GerenciadorPsicoEspiritualTest.cs
@@ -15,6 +15,53 @@ namespace PacienteVirtual.Tests
     public class GerenciadorPsicoEspiritualTest
     {
         [TestMethod()]
+        public void PV40AtualizarNecessidadesPsicoespirituaisValida()
+        {
+            long idConsultaVariavel = 100;
+            GerenciadorPsicoEspiritual gerenciadorPsicoEspiritual = GerenciadorPsicoEspiritual.GetInstance();
+            PsicoEspiritualModel psicoEspiritual = gerenciadorPsicoEspiritual.Obter(idConsultaVariavel);
+            Assert.IsNotNull(psicoEspiritual);
+            psicoEspiritual.IdConsultaVariavel = 100;
+            psicoEspiritual.Ansiedade = true;
+            psicoEspiritual.Apatico = false;
+            psicoEspiritual.BaixoAutoEstima = true;
+            psicoEspiritual.BuscaAssistenciaEspiritual = true;
+            psicoEspiritual.Choro = false;
+            psicoEspiritual.CrencaReligiosa = null;
+            psicoEspiritual.DisturbiosSono = false;
+            psicoEspiritual.EspecificaAssistenciaEspiritual = null;
+            psicoEspiritual.Estresse = true;
+            psicoEspiritual.PreocupacaoMorte = true;
+            psicoEspiritual.Raiva = true;
+
+            try
+            {
+                gerenciadorPsicoEspiritual.Atualizar(psicoEspiritual);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(NegocioException));
+            }
+
+            PsicoEspiritualModel psicoEspiritualAtualizado = gerenciadorPsicoEspiritual.Obter(idConsultaVariavel);
+
+            Assert.Equals(psicoEspiritualAtualizado.Ansiedade, false);
+            Assert.Equals(psicoEspiritualAtualizado.Apatico, false);
+            Assert.Equals(psicoEspiritualAtualizado.BaixoAutoEstima, false);
+            Assert.Equals(psicoEspiritualAtualizado.BuscaAssistenciaEspiritual, false);
+            Assert.Equals(psicoEspiritualAtualizado.Choro, false);
+            Assert.IsNull(psicoEspiritualAtualizado.CrencaReligiosa);
+            Assert.Equals(psicoEspiritualAtualizado.DisturbiosSono, false);
+            Assert.IsNull(psicoEspiritualAtualizado.EspecificaAssistenciaEspiritual);
+            Assert.Equals(psicoEspiritualAtualizado.Estresse, false);
+            Assert.Equals(psicoEspiritualAtualizado.HumorDeprimido, false);
+            Assert.Equals(psicoEspiritualAtualizado.IdConsultaVariavel, idConsultaVariavel);
+            Assert.Equals(psicoEspiritualAtualizado.Negacao, false);
+            Assert.Equals(psicoEspiritualAtualizado.PreocupacaoMorte, false);
+            Assert.Equals(psicoEspiritualAtualizado.Raiva, false);
+        }
+
+        [TestMethod()]
         public void PV41AtualizarNecessidadesPsicoespirituaisInvalida()
         {
             long idConsultaVariavel = 100;
